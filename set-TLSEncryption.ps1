7d267dcff1441ca8eeb9dbfc5071e7b75f32c483
Chris Shearer <cbshearer@gmail.com>
2020-01-23 08:29:19 -0800

    Update set-TLSEncryption.ps1
    Re-order protocols
    force creation of non-existent keys

--- set-TLSEncryption.ps1
+++ set-TLSEncryption.ps1
@@ -11,30 +11,6 @@
         New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -name 'DISABLEDByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
         Write-Host 'SSL 2.0 client has been DISABLED.'
 
-### Enable TLS 1.2
-    ## Server
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'ENABLED' -value '1' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'DISABLEDByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host 'TLS 1.2 server has been ENABLED.'
-    ## Client 
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'ENABLED' -value '1' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'DISABLEDByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host 'TLS 1.2 client has been ENABLED.'
-
-## Enable TLS 1.1
-    ## Server
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'ENABLED' -value '1' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'DISABLEDByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host 'TLS 1.1 server has been ENABLED.'
-    ## Client
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'ENABLED' -value '1' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'DISABLEDByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host 'TLS 1.1 client has been ENABLED.'
-
 ### Disable TLS 1.0
     ## Server
         New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Force | Out-Null
@@ -47,14 +23,43 @@
         New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
         Write-Host 'TLS 1.0 client been DISABLED.'
 
+### Enable Protocols
+### Enable TLS 1.1
+    ## Server
+        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'ENABLED' -value '1' -PropertyType 'DWord' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'DISABLEDByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+        Write-Host 'TLS 1.1 server has been ENABLED.'
+    ## Client
+        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'ENABLED' -value '1' -PropertyType 'DWord' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'DISABLEDByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+        Write-Host 'TLS 1.1 client has been ENABLED.'
+
+### Enable TLS 1.2
+    ## Server
+        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'ENABLED' -value '1' -PropertyType 'DWord' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'DISABLEDByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+        Write-Host 'TLS 1.2 server has been ENABLED.'
+    ## Client 
+        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'ENABLED' -value '1' -PropertyType 'DWord' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'DISABLEDByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+        Write-Host 'TLS 1.2 client has been ENABLED.'
+
+
 ### Disable old Ciphers
-    New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers' -Force | Out-Null
-    New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\DES 56`/56' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-    New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128`/128' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+    New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers'                -Force | Out-Null
+    New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\DES 56`/56'     -Force | Out-Null
+    New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128`/128'   -Force | Out-Null
+    New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168' -Force | Out-Null
+    New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\DES 56`/56'     -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+    New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128`/128'   -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
     New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
 
-### Set Cipher Functions
-### Values from https://www.grc.com/miscfiles/SChannel_Cipher_Suites.txt
+### Set Cipher Functions with values from https://www.grc.com/miscfiles/SChannel_Cipher_Suites.txt
 
 $FunctionValues = Invoke-WebRequest https://www.grc.com/miscfiles/SChannel_Cipher_Suites.txt
+
 Set-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Cryptography\Configuration\Local\SSL\00010002' -name "Functions" -value $FunctionValues.content -type string
