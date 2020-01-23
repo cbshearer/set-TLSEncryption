cb772160ceb5853b0e7de9790f1972426a518579
Chris Shearer <cbshearer@gmail.com>
2020-01-23 12:41:44 -0800

    Update set-TLSEncryption.ps1
    fixing typos
    changed some logic

--- set-TLSEncryption.ps1
+++ set-TLSEncryption.ps1
@@ -21,7 +21,7 @@
         New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Force | Out-Null
         New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
         New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host 'TLS 1.0 client been DISABLED.'
+        Write-Host 'TLS 1.0 client has been DISABLED.'
 
 ### Enable Protocols
 ### TLS 1.1
@@ -52,16 +52,48 @@
 ### Disable old Ciphers
     ## Make sure keys exist
         New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers'                -Force | Out-Null
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\DES 56`/56'     -Force | Out-Null
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128`/128'   -Force | Out-Null
+        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\DES 56/56'     -Force | Out-Null
+        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128/128'   -Force | Out-Null
         New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168' -Force | Out-Null
     ## Set values
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\DES 56`/56'     -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128`/128'   -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\DES 56/56'     -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128/128'   -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
         New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
 
 ### Set Cipher Functions with values from https://www.grc.com/miscfiles/SChannel_Cipher_Suites.txt
+    $FunctionValues = $null
+    $FunctionValues = Invoke-WebRequest https://www.grc.com/miscfiles/SChannel_Cipher_Suites.txt | Out-Null
+    $FunctionValues = $FunctionValues.content
 
-$FunctionValues = Invoke-WebRequest https://www.grc.com/miscfiles/SChannel_Cipher_Suites.txt
+if (!($FunctionValues))
+    {$FunctionValues = "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384_P384,
+TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA_P384,
+TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA_P384,
+TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA_P256,
+TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA_P256,
+TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256_P384,
+TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256_P384,
+TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256_P256,
+TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256_P256,
+TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P384,
+TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P256,
+TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA_P384,
+TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA_P256,
+TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256_P384,
+TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256_P256,
+TLS_DHE_DSS_WITH_AES_256_CBC_SHA256,
+TLS_DHE_DSS_WITH_AES_256_CBC_SHA,
+TLS_DHE_DSS_WITH_AES_128_CBC_SHA256,
+TLS_DHE_DSS_WITH_AES_128_CBC_SHA,
+TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA,
+TLS_RSA_WITH_AES_256_CBC_SHA256,
+TLS_RSA_WITH_AES_256_CBC_SHA,
+TLS_RSA_WITH_AES_128_CBC_SHA256,
+TLS_RSA_WITH_AES_128_CBC_SHA,
+TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+SSL_CK_DES_192_EDE3_CBC_WITH_MD5"
 
-Set-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Cryptography\Configuration\Local\SSL\00010002' -name "Functions" -value $FunctionValues.content -type string
+    Set-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Cryptography\Configuration\Local\SSL\00010002' -name "Functions" -value $FunctionValues -type string
+}
+
+else {Set-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Cryptography\Configuration\Local\SSL\00010002' -name "Functions" -value $FunctionValues.content -type string}
